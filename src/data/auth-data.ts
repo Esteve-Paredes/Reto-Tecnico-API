1bdc2616cde8d420a544ccd10ef30d13f2764058
esteve <esteve.paredes.4@gmail.com>
2024-02-16 10:13:01 -0500

    Fix function validataCredentials

--- src/data/auth-data.ts
+++ src/data/auth-data.ts
@@ -1,5 +1,10 @@
 import { query } from "../db/index";
 
-export async function getUserByEmail(email: string) {
-  return (await query("SELECT * FROM users WHERE email = $1", [email])).rows[0];
+export async function getUserByEmail(email: string, password: string) {
+  return (
+    await query("SELECT * FROM users WHERE email = $1 AND password = $2", [
+      email,
+      password,
+    ])
+  ).rows[0];
 }
--- src/services/auth.services.ts
+++ src/services/auth.services.ts
@@ -3,7 +3,7 @@ import { User, userParams } from "../models/user";
 
 export async function validateCredentials(userData: userParams): Promise<User> {
   const { email, password } = userData;
-  const user = await userDB.getUserByEmail(email);
+  const user = await userDB.getUserByEmail(email, password);
 
   return user;
 }
